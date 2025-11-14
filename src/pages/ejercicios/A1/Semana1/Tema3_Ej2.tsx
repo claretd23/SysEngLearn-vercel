d7ae18aa1442a8ac5de693df93606ce11454d860
claretd23 <mariaclaretdelgadomacias@gmail.com>
2025-11-13 23:20:57 -0600

    modificacion de error en ejercicio

--- src/pages/ejercicios/A1/Semana1/Tema3_Ej2.tsx
+++ src/pages/ejercicios/A1/Semana1/Tema3_Ej2.tsx
@@ -86,7 +86,7 @@ export default function Tema1_Ej2_Ordinals() {
       setRespuesta("Correct");
       setCorrectas((prev) => prev + 1);
     } else {
-      setRespuesta('Incorrect. The correct answer is "${actual.correcta}".');
+      setRespuesta(`Incorrect. The answer is "${actual.correcta}".`);
     }
   };
 

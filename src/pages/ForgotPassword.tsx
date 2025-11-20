fea43c13a4b5c22a4b2e582b54c4cb8dfd4a9714
claretd23 <mariaclaretdelgadomacias@gmail.com>
2025-11-20 14:15:16 -0600

    se modificaron ejercicio sem12

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,5 +1,5 @@
 import '../styles/ForgotPassword.css';
-import logo from "../../assets/LOGO.svg";
+import logo from "../../assets/LOGODA.svg";
 import { useState } from 'react';
 import axios from 'axios';
 
